1c3e1d85010c609355efb3c42f2dba619078754a
wjjeong <nttejun@gmail.com>
2019-06-05 22:54:22 -0700

    Spring init setting

---
+++ src/main/java/spring/web/service/controller/BoardController.java
@@ -0,0 +1,18 @@
+package spring.web.service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping("/")
+    public String boardPage(){
+        return "board/main";
+    }
+
+
+}
+
---
+++ src/main/webapp/WEB-INF/views/board/main.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wjjeong
+  Date: 05/06/2019
+  Time: 10:35 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form method="post">
+
+</form>
+</body>
+</html>
