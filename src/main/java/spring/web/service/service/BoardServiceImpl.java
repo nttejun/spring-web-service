c07f6943e173ffaf6e59580e6da7502d4ed93201
wjjeong <nttejun@gmail.com>
2019-06-06 21:27:40 -0700

    Bean setting

--- src/main/java/spring/web/service/service/BoardServiceImpl.java
+++ src/main/java/spring/web/service/service/BoardServiceImpl.java
@@ -1,11 +1,13 @@
 package spring.web.service.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.web.service.domain.Board;
 
 import spring.mappers.BoardMapper;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import java.util.List;
 
@@ -14,7 +16,6 @@ public class BoardServiceImpl implements BoardService{
 
     @Inject
     private BoardMapper boardMapper;
-
 
     @Transactional
     @Override
